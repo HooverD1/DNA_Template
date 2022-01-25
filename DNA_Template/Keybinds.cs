64dd5043fcc1f46591023b276f5cd4336744f321
David Hoover <davehoover1985@gmail.com>
2022-01-25 14:58:51 -0600

    Added template classes: MyAddIn, Keybinds, UDFs, Ribbon. Still requires examples and ribbon implementation.

--- DNA_Template/Keybinds.cs
+++ DNA_Template/Keybinds.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DNA_Template
 {
-    public class Class1
+    class Keybinds
     {
     }
 }
---
+++ DNA_Template/MyAddIn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+
+namespace DNA_Template
+{
+    public class MyAddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+
+        }
+
+        public void AutoClose()
+        {
+            
+        }
+
+        public void xlAutoClose()
+        {
+
+        }
+        
+    }
+}
